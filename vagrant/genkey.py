0be2ad091091e4d06a24060f836dd8eb97946c57
jdn13001 <jared.nye@uconn.edu>
2020-03-26 21:32:35 -0400

    changing key variable to hex only

--- vagrant/genkey.py
+++ vagrant/genkey.py
@@ -5,16 +5,16 @@ import sys
 KEY_SIZE = 16
 
 
-def get_hex(val):
-    return ''.join('{:02x}'.format(x) for x in val)
+#def get_hex(val):
+#    return ''.join('{:02x}'.format(x) for x in val)
 
 
 def main():
-    key = os.urandom(KEY_SIZE)
+    key = os.urandom(KEY_SIZE).hex()
 
     #commands = f'export DRM_KEY={get_hex(key)}\n'
     #sys.stdout.write(commands)
-    print(get_hex(key))
+    print(key)
 
 if __name__ == '__main__':
     main()
