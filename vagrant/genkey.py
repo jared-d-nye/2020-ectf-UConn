a9541320e6829984181d74dd2c8d3fcf2e339fac
jdn13001 <jared.nye@uconn.edu>
2020-03-30 12:53:31 -0400

    updating conversion of bytes to c for createDevice

--- vagrant/genkey.py
+++ vagrant/genkey.py
@@ -4,15 +4,15 @@ import sys
 # Size in bytes
 KEY_SIZE = 16
 
-#def get_hex(val):
-#    return ''.join('{:02x}'.format(x) for x in val)
+def get_hex(val):
+    return ''.join('{:02x}'.format(x) for x in val)
 
 def main():
     key = os.urandom(KEY_SIZE).hex()
 
     #commands = f'export DRM_KEY={get_hex(key)}\n'
     #sys.stdout.write(commands)
-    print(key)
+    print(get_hex(key))
 
 if __name__ == '__main__':
     main()
